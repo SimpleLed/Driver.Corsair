08cf3dea98970e442a4c73f8c838ea8f2efea9ee
James Johnston <james@ezmuze.co.uk>
2020-10-03 20:50:41 +0100

    update version number

--- Driver.Corsair/CUEDriver.cs
+++ Driver.Corsair/CUEDriver.cs
@@ -744,7 +744,7 @@ namespace Driver.Corsair
                 Id = Guid.Parse("59440d02-8ca3-4e35-a9a3-88b024cc0e2d"),
                 Author = "Fanman03",
                 Blurb = "Driver for all devices compatible with the iCUE SDK.",
-                CurrentVersion = new ReleaseNumber(1,0,0,7),
+                CurrentVersion = new ReleaseNumber(1,0,0,8),
                 GitHubLink = "https://github.com/SimpleLed/Driver.Corsair",
                 IsPublicRelease = true
             };
