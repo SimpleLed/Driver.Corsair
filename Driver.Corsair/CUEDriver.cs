e11db3c2cfab114a1564a3cf346a297e8a102189
James Johnston <james@ezmuze.co.uk>
2021-01-30 23:27:04 +0000

    bump version

--- Driver.Corsair/CUEDriver.cs
+++ Driver.Corsair/CUEDriver.cs
@@ -951,13 +951,12 @@ namespace Driver.Corsair
                 Id = Guid.Parse("59440d02-8ca3-4e35-a9a3-88b024cc0e2d"),
                 Author = "Fanman03",
                 Blurb = "Driver for all devices compatible with the iCUE SDK.",
-                CurrentVersion = new ReleaseNumber(1,0,0,21),
+                CurrentVersion = new ReleaseNumber(1,0,0,22),
                 GitHubLink = "https://github.com/SimpleLed/Driver.Corsair",
                 IsPublicRelease = true,
                 ProductCategory = ProductCategory.Hardware,
                 SetDeviceOverride = SetDeviceOverride,
                 GetCustomDeviceSpecifications = GetCustomDeviceSpecifications
-
             };
         }
 
