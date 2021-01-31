a638d9851fdf7a7041e6c3f05da2ae5d4c68d9f9
James Johnston <james@ezmuze.co.uk>
2021-01-31 00:04:09 +0000

    rename auto rev var

--- Driver.Corsair/revision.cs
+++ Driver.Corsair/revision.cs
@@ -1,1 +1,1 @@
-namespace Revision { public static class BuildRev { public static int BuildRevision = 34; } }
+namespace AutoRevision { public static class BuildRev { public static int BuildRevision = 34; } }
